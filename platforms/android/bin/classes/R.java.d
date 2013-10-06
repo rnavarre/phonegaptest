e8bb739756e7f2e700fe7e2f1d37a30281dfea7f
rnavarre <ronald_navarrete@yahoo.es>
2013-10-05 21:45:23 -0500

    Subir nuevamente para prueba

---
+++ platforms/android/bin/classes/R.java.d
@@ -0,0 +1,9 @@
+D:\WorkspacePhonegap\phonegaptest\platforms\android\gen\com\ejemplo\hola\R.java \
+ : D:\WorkspacePhonegap\phonegaptest\platforms\android\res\drawable\icon.png \
+D:\WorkspacePhonegap\phonegaptest\platforms\android\res\drawable-hdpi\icon.png \
+D:\WorkspacePhonegap\phonegaptest\platforms\android\res\drawable-ldpi\icon.png \
+D:\WorkspacePhonegap\phonegaptest\platforms\android\res\drawable-mdpi\icon.png \
+D:\WorkspacePhonegap\phonegaptest\platforms\android\res\drawable-xhdpi\icon.png \
+D:\WorkspacePhonegap\phonegaptest\platforms\android\res\values\strings.xml \
+D:\WorkspacePhonegap\phonegaptest\platforms\android\res\xml\config.xml \
+D:\WorkspacePhonegap\phonegaptest\platforms\android\bin\AndroidManifest.xml \
